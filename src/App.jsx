739b47595c8f2c65d335e31dbca1be17a605b399
Carlos Garcia <mrgarcia7@gmail.com>
2023-11-14 22:39:33 -0500

    feat: :sparkles: create responsive menu with hamburguer icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { AppBar, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setDrawerOpen(open);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <AppBar position="fixed">
+        <Toolbar>
+          {drawerOpen ? (
+            // Show the close icon when the drawer is open
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="close menu"
+              onClick={toggleDrawer(false)}
+            >
+              <CloseIcon />
+            </IconButton>
+          ) : (
+            // Show the menu icon when the drawer is closed
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+        ModalProps={{ keepMounted: true }}
+        style={{ position: 'absolute', zIndex: 1 }}
+      >
+        <List>
+          <ListItemButton>
+            <ListItemText primary="Item 1" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemText primary="Item 2" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemText primary="Item 3" />
+          </ListItemButton>
+          {/* Add more ListItem components as needed */}
+        </List>
+      </Drawer>
+
+      {/* Add a margin to the main content to avoid being covered by the fixed AppBar */}
+      <div style={{ marginTop: '64px', padding: '20px' }}>
+        <h1>Main Content</h1>
+        {/* Add your content here */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
+
 }
 
 export default App
